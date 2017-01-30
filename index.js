281ce6dd4b0eb5f645be5960e5ad3f8d612a2e69
justin <justin_brower@brown.edu>
2017-01-29 19:52:10 -0500

    Focus text area automatically

--- index.js
+++ index.js
@@ -19,40 +19,40 @@ function evaluate(cmd) {
 
     var hardCode = {
         help: "commands: \n\
-                hours - View TA hours.\n\
-                [h]omeworks - View homeworks.\n\
-                [p]rojects - View projects\n\
-                [s]taff - Meet the staff\n\
-                news - Stuff that's going on\n\
-                show <assignment> - Show the pdf of an assignment\n",
+    hours - View TA hours.\n\
+    [h]omeworks - View homeworks.\n\
+    [p]rojects - View projects\n\
+    [s]taff - Meet the staff\n\
+    news - Stuff that's going on\n\
+    show <assignment> - Show the pdf of an assignment\n",
         
         projects: "projects: \n\
-                (162 only) warmup\t\t\t1/13 - 2/9\n\
-                crypto           \t\t\t2/2 - 2/16\n\
-                flag             \t\t\t2/16 - 3/9\n\
-                handin           \t\t\t3/9 - 4/6\n\
-                dropbox          \t\t\t4/6 - 4/20, 5/4, 5/11\n", 
+    (162 only) warmup\t\t\t1/13 - 2/9\n\
+    crypto           \t\t\t2/2 - 2/16\n\
+    flag             \t\t\t2/16 - 3/9\n\
+    handin           \t\t\t3/9 - 4/6\n\
+    dropbox          \t\t\t4/6 - 4/20, 5/4, 5/11\n", 
 
 
 
         staff: "\n\
-                zstoll: Zoe Stoll (HTA)\n\
-                agokasla: Aaron Gokaslan (HTA)\n\
-                jbrower: Justin Brower\n\
-                arothen: Anne Rothen\n\
-                zdixon: Zachary Dixon\n\
-                arothen: Anne Rothen\n\
-                memo: Memo Beltran\n", 
+    zstoll: Zoe Stoll (HTA)\n\
+    agokasla: Aaron Gokaslan (HTA)\n\
+    jbrower: Justin Brower\n\
+    arothen: Anne Rothen\n\
+    zdixon: Zachary Dixon\n\
+    arothen: Anne Rothen\n\
+    memo: Memo Beltran\n", 
 
         homeworks: "homeworks: \n\
-                hw01      \t\t\t1/30 - 2/6\n\
-                hw02      \t\t\t2/6 - 2/13\n\
-                hw03      \t\t\t2/13 - 2/20\n\
-                hw04      \t\t\t2/20 - 2/27\n\
-                hw05      \t\t\t3/6 - 3/13\n\
-                hw06      \t\t\t3/13 - 3/20\n\
-                hw07      \t\t\t3/20 - 4/10\n\
-                hw05      \t\t\t4/10 - 4/17\n", 
+    hw01      \t\t\t1/30 - 2/6\n\
+    hw02      \t\t\t2/6 - 2/13\n\
+    hw03      \t\t\t2/13 - 2/20\n\
+    hw04      \t\t\t2/20 - 2/27\n\
+    hw05      \t\t\t3/6 - 3/13\n\
+    hw06      \t\t\t3/13 - 3/20\n\
+    hw07      \t\t\t3/20 - 4/10\n\
+    hw05      \t\t\t4/10 - 4/17\n", 
     };
 
     // install shortcuts.
@@ -108,4 +108,5 @@ jQuery(function($, undefined) {
 $( document ).ready(function() {
   // Handler for .ready() called.
   jQuery.terminal.active().echo(evaluate("help"));
+  $("textarea").trigger("focus");
 });
