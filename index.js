0b39842595fa1eecd3bbc06b996c270303fc32b8
justin <justin_brower@brown.edu>
2017-01-31 14:08:07 -0500

    added leave command, email command, etc.

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ function redirectTo(link) {
         window.focus();
 }
 
+function sendEmail(to) {
+
+    var email;
+
+    // assume 'to' is a login.
+    if (to == "tas") {
+        email = "cs1660tas@lists.brown.edu"
+    } else if (to == "htas") {
+        email = "cs1660htas@lists.brown.edu"
+    } else if (to == "roberto") {
+        email = "roberto_tamassia@brown.edu";
+    } else {
+        email = to + "@cs.brown.edu";
+    } 
+
+    var subject = "CS166 - Question";
+    var message = "?";
+
+   var str = 'http://mail.google.com/mail/?view=cm&fs=1'+
+  '&to=' + email +
+  '&su=' + subject +
+  '&body=' + message +
+  '&ui=1';
+    window.open(str);
+}
+
 function linkForAssignmentOrProject(what) {
     var assgn = what + ".pdf";
     
@@ -23,36 +49,45 @@ function evaluate(cmd) {
     [h]omeworks - View homeworks.\n\
     [p]rojects - View projects\n\
     [s]taff - Meet the staff\n\
+    leave - Go to a more normal website\n\
     news - Stuff that's going on\n\
-    show <assignment> - Show the pdf of an assignment\n",
+    show <assignment> - Show the pdf of an assignment\n\r\
+    email <login | tas | htas> - Send an email to one of the instructors.",
         
         projects: "projects: \n\
-    (162 only) warmup\t\t\t1/13 - 2/9\n\
-    crypto           \t\t\t2/2 - 2/16\n\
-    flag             \t\t\t2/16 - 3/9\n\
-    handin           \t\t\t3/9 - 4/6\n\
-    dropbox          \t\t\t4/6 - 4/20, 5/4, 5/11\n", 
+    *(162 only) warmup \t\t\t1/13 - 2/9\n\
+    crypto             \t\t\t2/2 - 2/16\n\
+    flag               \t\t\t2/16 - 3/9\n\
+    handin             \t\t\t3/9 - 4/6\n\
+    dropbox            \t\t\t4/6 - 4/20, 5/4, 5/11\n\
+    \n\
+    * indicates that a project has been released.", 
 
+        news: "\nNone yet. Check back soon.\n",
 
+        hours: "\nThese will be up soon.\n",
 
         staff: "\n\
+    roberto: Roberto Tamassia (Prof)\n\
     zstoll: Zoe Stoll (HTA)\n\
     agokasla: Aaron Gokaslan (HTA)\n\
     jbrower: Justin Brower\n\
     arothen: Anne Rothen\n\
     zdixon: Zachary Dixon\n\
     arothen: Anne Rothen\n\
-    memo: Memo Beltran\n", 
+    gbeltran: Memo Beltran\n", 
 
         homeworks: "homeworks: \n\
-    hw01      \t\t\t1/30 - 2/6\n\
+    *hw01     \t\t\t1/30 - 2/6\n\
     hw02      \t\t\t2/6 - 2/13\n\
     hw03      \t\t\t2/13 - 2/20\n\
     hw04      \t\t\t2/20 - 2/27\n\
     hw05      \t\t\t3/6 - 3/13\n\
     hw06      \t\t\t3/13 - 3/20\n\
     hw07      \t\t\t3/20 - 4/10\n\
-    hw05      \t\t\t4/10 - 4/17\n", 
+    hw05      \t\t\t4/10 - 4/17\n\
+    \n\
+    * indicates that a homework has been released.\n", 
     };
 
     // install shortcuts.
@@ -75,6 +110,13 @@ function evaluate(cmd) {
                     redirectTo(linkForAssignmentOrProject(what));
                 }
                 return "";
+            } else if (cmd === "email") {
+                var who = parts[1];
+                if (who) {
+                    sendEmail(who);
+                }
+            } else if (cmd == "leave") {
+                location.href = "https://cs.brown.edu/courses/cs166/old"
             }
         }
 
